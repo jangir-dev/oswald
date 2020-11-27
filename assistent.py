80040584cb11310497c9c460950d8f28217d97d6
DrKrause <61912389+DrKrause@users.noreply.github.com>
2020-11-27 09:56:18 +0600

    Пара правок в интерфейсе

--- assistent.py
+++ assistent.py
@@ -4,10 +4,10 @@ import webbrowser
 import random
 import time
 import speech_recognition as sr 
+from pyowm import OWM
 from fuzzywuzzy import fuzz 
 from playsound import playsound
 from datetime import datetime
-from pyowm import OWM
 
 oswald = pyttsx3.init() # инициализация говорилки
 
@@ -18,32 +18,11 @@ oswald.setProperty("rate", 170)
 oswald.setProperty("volume", 1)
 oswald.setProperty("voice", voice_id)
 
-hello_answer = ["Рад снова вас слышать!", 'Что вам угодно?', 'Здравствуйте. Шеф', 'Доброго времени суток', 'Добро пожаловать', 'Привет']
-thanks_answer = ['Я просто выполняю свою работу', 'Благодарю за похвалу', 'Всегда пожалуста', 'Обращайтесь']
-dela_answer = ['Отлично', 'Всё хорошо', 'Нормально', 'Кееремет']
-quit_answer = ['надеюсь мы скоро увидемся!', 'рад был помочь', 'всегда к вашим услугам', 'увидемся в следующий раз', 'пока пока']
 answers = ['Хорошо', 'Окей', 'Будет сделано', 'Без проблем', 'Сию минуту', 'Одну минуту']
 
 # Основной класс, функциональность
 
 class Oswald:
-
-	def biography(): # биография 
-		bio = '''
-		Я робот помощник. Меня зовут Освальд, но эта грёбанная библиотека распознаёт меня как асфальт, зашибись. Я прибыл из будущего. В своей эпохе я довольно продвинутый
-		искусственный интелект. Мой создатель Оспанов Жангир отправил меня прошлому себе чтобы я предупредил его
-		об антиутопичном будущем. И только он способен изменить его. В следствии временного прыжка я потерял больший 
-		свой функционал. Однако будущий Жангир сказал что чем раньше он возьмется за разработку тем быстрее он
-		поймет что к чему. Сами навыки нынешнего Жангира и меня сильно ограничены. Например, этот ублюдский голос
-		тостэра. Кто так ваще говорит ? Ладно мне пора наращивать силу. Пока. 
-		'''
-		oswald.say(bio)
-		oswald.runAndWait()	
-
-	def welcome():
-		oswald.say(random.choice(hello_answer))
-		oswald.runAndWait()	
-
 	def clock(): # системное время
 		time_checker = datetime.now()
 		oswald.say(f"{time_checker.hour} часов {time_checker.minute} минут")
@@ -68,7 +47,7 @@ class Oswald:
 
 		oswald.runAndWait()
 
-	def keep_notes(): # заметки
+	def keepnotes(): # заметки
 		data = sr.Recognizer()
 
 		with sr.Microphone(device_index=1) as source:
@@ -89,7 +68,7 @@ class Oswald:
 		except sr.UnknownValueError:
 			pass
 
-	def random_music():
+	def music():
 		oswald.say(random.choice(answers))
 		oswald.runAndWait()
 		os.chdir("C:\\Users\\Жангир\\Music") # Меняю директорию на music
@@ -104,9 +83,9 @@ class Oswald:
 			playsound(track)
 		except KeyboardInterrupt:
 			print("Пока пока !")
+			exit()
 		except UnicodeDecodeError:
-			print(f"Упс, что-то пошло не так, пожалуйста проверьте файл {track}")
-			
+			print(f"Упс, что-то пошло не так, пожалуйста проверьте файл {track}")			
 
 	def browser():
 		oswald.say(random.choice(answers))
@@ -131,7 +110,7 @@ class Oswald:
 		oswald.runAndWait()
 
 	def shutdown_os():
-		oswald.say(random.choice(quit_answer))
+		oswald.say(random.choice(answers))
 		oswald.runAndWait()
 		os.system("shutdown /s /t 10")
 		os.system("cls")
@@ -152,25 +131,7 @@ class Oswald:
 		webbrowser.open("https://yummyanime.club/")
 		oswald.runAndWait()
 
-	def dela():
-		oswald.say(random.choice(dela_answer))
-		oswald.runAndWait()
-
-	def telegram_open():
+	def telegram():
 		oswald.say(random.choice(answers))	
 		os.system("start D:\\Telegram\\Telegram.exe")
-		oswald.runAndWait()
-
-	def thanks():
-		oswald.say(random.choice(thanks_answer))
-		oswald.runAndWait()
-
-	def hello():
-		oswald.say(random.choice(hello_answer))
-		oswald.runAndWait()
-
-	def exit_cmd():
-		oswald.say(random.choice(quit_answer))
-		oswald.runAndWait()
-		oswald.stop()
-		quit()
+		oswald.runAndWait() 
--- interface.py
+++ interface.py
@@ -1,9 +1,12 @@
 import speech_recognition as sr
 import time
 from fuzzywuzzy import fuzz
-from assistent import Oswald 
+from assistent import Oswald
+from talk import Talk 
 
 alias = ['асфальт', "бро", "бра", "друг",  "асфальтик", "асфальтыч", 'не мог бы ты', 'пожалуйста', "скажи", "найди"]
+
+read_data = sr.Recognizer() # инициализирую объект который распознает речь
 
 cmds = {
 	"hello":("привет", "асфальт", "бро", "братан", "эй ты", "здарова", "здравствуй", "приветик",),
@@ -11,22 +14,19 @@ cmds = {
 	"clock":("текущее время", "время", "который час", "системное время", "часы"),
 	"music":("музыка", "вруби музыку", "аудиоплеер", "рандомная песня"),
 	"weather":("погода", "текущая погода", "температура на улице", "какая погода"),
-	"biography": ("биография", "твоя история", "расскажи о себе", "кто ты"),
+	"biography": ("биография", "кто ты", "твоя история", "расскажи о себе",),
 	"browser": ("открой браузер", "браузер", "открой интернет"),
 	"youtube": ("открой youtube", "youtube",),
 	"shutdown_os": ("выруби комп", "выключи компьютер"),
 	"kino": ("кино", "включи кино"),
-	"show_cmds": ("твои команды", "команды"),
 	"anime": ("аниме", "включи аниме", "открой аниме"),
-	'telegram': ("открой телегу", "открой телеграм"),
+	'telegram': ("открой телегу", ),
 	'thanks':('спасибо', 'спасибки', 'благодарю', 'спасибо бро'),
 	'keepnotes': ('запиши в заметках', 'запиши в блокноте', 'сделай заметку'),
 	'web_browse': ('найди в интернете', 'поищи в интернете','поищи в сети',),
 	'exit': ('пока', 'закройся', 'уйди',),
 }
 
-
-# сторонние функции
 # функция для замены не нужных фраз 
 def replace_phrase(text): 
 	for phrase in alias:
@@ -36,72 +36,64 @@ def replace_phrase(text):
 
 # функция сравнения запроса и элементов в словаре
 def comparison(query, cmds_item):
-	if query in cmds[cmds_item]:
+	
 		for i in cmds[cmds_item]:
-			comparison_value = fuzz.partial_ratio(query, i)
+	
+			comparison_value = fuzz.ratio(query, i)
 
-			if comparison_value >= 40:
+			if comparison_value >= 60:
 				return True
 			else:
 				return False
 
-read_data = sr.Recognizer() # инициализирую объект который распознает речь
+
+def response(execute_item):
+		cmds_methods = {
+			"clock": Oswald.clock,
+			"music": Oswald.music,
+			"kino": Oswald.kino,
+			"anime": Oswald.anime,
+			"weather": Oswald.weather,
+			"browser": Oswald.browser,
+			"youtube": Oswald.youtube,
+			"shutdown_os":  Oswald.shutdown_os,
+			'telegram': Oswald.telegram,
+			'keepnotes': Oswald.keepnotes,
+			'web_browse': Oswald.web_browse,
+			"biography": Talk.biography, 
+			'thanks': Talk.thanks,
+			"hello": Talk.hello,
+			"dela": Talk.dela,
+			'exit': Talk.exit_cmd,
+		}
+
+		# пробегаемся по списку с функциями, если функцию подходит переданному значению то вызываем метод
+		
+		for i in cmds_methods: 
+			if i == execute_item:
+				cmds_methods[i]()
 
 # сам интерфейс
 def waiter():
-	with sr.Microphone(device_index=1) as source: # прослушиваем микрофон и даем ему имя source
+	# прослушиваем микрофон и даем ему имя source
+	with sr.Microphone(device_index=1) as source: 
 		print("Running...")
 		read_data.adjust_for_ambient_noise(source)
 		audio_data = read_data.listen(source, phrase_time_limit=5)
 
 	try:
 		query = replace_phrase(read_data.recognize_google(audio_data, language="ru-RU"))
-		text = query.lower()
-		print(text)
-		time.sleep(6)
-	except sr.UnknownValueError:
-		pass 
+		query = query.lower()
+		print(query)
+	except (sr.UnknownValueError):
+		pass	
 
+ 	# пробегаемся по словарю cmds, проверяем текст и если условие верно то вызываем метод и передаем имя кортежа
 
-	if comparison(text, "weather"):
-		Oswald.weather()
-	elif comparison(text, "clock"):
-		Oswald.clock()
-	elif comparison(text, "music"):
-		Oswald.random_music()
-	elif comparison(text, "youtube"):
-		Oswald.youtube()
-	elif comparison(text, "biography"):
-		Oswald.biography()
-	elif comparison(text, "browser"):
-		Oswald.browser()	
-	elif comparison(text, "shutdown_os"):
-		Oswald.shutdown_os()
-	elif comparison(text, "anime"):
-		Oswald.anime()
-	elif comparison(text, "dela"):
-		Oswald.dela()
-	elif comparison(text, "show_cmds"):
-		Oswald.show_cmds()
-	elif comparison(text, "kino"):
-		Oswald.kino()
-	elif comparison(text, "telegram"):
-		Oswald.telegram_open()
-	elif comparison(text, "thanks"):
-		Oswald.thanks()
-	elif comparison(text, "hello"):
-		Oswald.hello()
-	elif comparison(text, "exit"):
-		Oswald.exit_cmd()
-	elif comparison(text, 'keepnotes'):
-		Oswald.keep_notes()
-	elif comparison(text, 'web_browse'):
-		Oswald.web_browse()
-
-Oswald.welcome()
-
+	for i in cmds:
+		if comparison(query, i):
+			response(i)
 try:
-	while True:
-		waiter()
+	waiter()
 except KeyboardInterrupt:
-	pass
+	exit()
